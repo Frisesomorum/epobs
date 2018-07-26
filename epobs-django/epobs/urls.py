8c1909ad3d0289316fd0e85725f106f473fecc20
Frisesomorum <timyhu@gmail.com>
2018-07-26 11:41:17 -0700

    Start POC of edit-students view

--- epobs-django/epobs/urls.py
+++ epobs-django/epobs/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from students import views as studentViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('students/', studentViews.edit, name='editStudent')
 ]
