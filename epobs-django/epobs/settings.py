e47e75819c151cc44d2e93619e92d02e82f08ddc
Frisesomorum <timyhu@gmail.com>
2019-04-16 14:54:42 -0700

    Change default date format and language code

--- epobs-django/epobs/settings.py
+++ epobs-django/epobs/settings.py
@@ -126,15 +126,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gh'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
+
+DATE_FORMAT = 'j\/n\/Y'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'school-select'
